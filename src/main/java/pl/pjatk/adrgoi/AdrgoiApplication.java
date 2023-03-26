262f9a47e982d738ee7b36da3f9da20dfba71cd1
Adrian Goik <s23970@pjwstk.edu.pl>
2023-03-26 14:18:59 +0200

    Initial commit

---
+++ src/main/java/pl/pjatk/adrgoi/AdrgoiApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.adrgoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdrgoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdrgoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/adrgoi/AdrgoiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.adrgoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdrgoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
